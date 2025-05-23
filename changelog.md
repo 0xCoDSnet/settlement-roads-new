9d9d6345770ff6b46cf9a541cfec42177e457d2d
Coun7ered <erik.bondel@gmx.de>
2025-05-23 19:03:10 +0200

    update to version 2.0.1 with road averaging improvements and new decoration features

---
+++ changelog.md
@@ -0,0 +1,5 @@
+*2.0.1*
+***
+Fix: 
+
+- Small road averaging improvement
--- src/main/java/net/countered/settlementroads/SettlementRoads.java
+++ src/main/java/net/countered/settlementroads/SettlementRoads.java
@@ -2,14 +2,9 @@ package net.countered.settlementroads;
 
 import net.countered.settlementroads.config.ModConfig;
 import net.countered.settlementroads.events.ModEventHandler;
-import net.countered.settlementroads.features.RoadFeature;
+import net.countered.settlementroads.features.config.RoadFeatureRegistry;
+import net.countered.settlementroads.persistence.attachments.WorldDataAttachment;
 import net.fabricmc.api.ModInitializer;
-
-import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
-import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
-import net.minecraft.registry.*;
-import net.minecraft.util.Identifier;
-import net.minecraft.world.gen.GenerationStep;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,36 +12,24 @@ public class SettlementRoads implements ModInitializer {
 
 	public static final String MOD_ID = "settlement-roads";
 
-	public static final Logger LOGGER = LoggerFactory.getLogger(SettlementRoads.MOD_ID);
+	private static final Logger LOGGER = LoggerFactory.getLogger(SettlementRoads.MOD_ID);
 
-	// TODO
-	// IMPORTANT
+	// -5233360391469774945
+
+	// Fix:
+	// Clean snow from roads
 
 	// OPTIONAL
-	// Possibly broken roads when recaching on world reload?
-	// Biome specific road changes
 	// Location lag reducing (async locator?)/ structure essentials / place instant roads?
 	// Bridges
 	// Tunnels
-	// fix roads on snow icebergs
-	// Remove placed blocks from caches
-	// place slabs on artificial roads
 
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Initializing Settlement Roads...");
+		WorldDataAttachment.registerWorldDataAttachment();
 		ModConfig.init(MOD_ID, ModConfig.class);
-		registerFeatures();
+		RoadFeatureRegistry.registerFeatures();
 		ModEventHandler.register();
-	}
-
-	private void registerFeatures() {
-		LOGGER.info("Registering Features...");
-		Registry.register(Registries.FEATURE, Identifier.of(MOD_ID, "road_feature"), RoadFeature.ROAD_FEATURE);
-		BiomeModifications.addFeature(
-				BiomeSelectors.all(),
-				GenerationStep.Feature.UNDERGROUND_STRUCTURES,
-				RoadFeature.ROAD_FEATURE_PLACED_KEY
-		);
 	}
 }
